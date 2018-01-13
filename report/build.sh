d7eab09ba149533b45c2a4e062ef3bd58e6ec745
BOT_Axel <axellebot@gmail.com>
2018-01-13 17:40:48 +0100

    [TASK] Refactored latex skeleton
    - Edited gitignore
    - Edited images
    - Edited build script

--- report/build.sh
+++ report/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+FILE_NAME_INPUT="main"
+OUTPUT_DIRECTORY="build"
+echo "Building ..."
+mkdir "./$OUTPUT_DIRECTORY"
+pdflatex -output-directory=$OUTPUT_DIRECTORY --shell-escape $FILE_NAME_INPUT
+makeindex "./$OUTPUT_DIRECTORY/$FILE_NAME_INPUT.idx" -s "./$OUTPUT_DIRECTORY/StyleInd.ist"
+biber -aux-directory=$OUTPUT_DIRECTORY main
+pdflatex -output-directory=$OUTPUT_DIRECTORY --shell-escape $FILE_NAME_INPUT
 
-pdflatex --shell-escape main
-makeindex main.idx -s StyleInd.ist
-biber main
-pdflatex --shell-escape main
+# Cleaning
+mv "./$OUTPUT_DIRECTORY/$FILE_NAME_INPUT.pdf" "./$FILE_NAME_INPUT.pdf"
