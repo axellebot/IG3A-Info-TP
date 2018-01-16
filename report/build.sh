b669278d2fe17546aec451c4e21e810548c14991
BOT_Axel <axellebot@gmail.com>
2018-01-16 10:30:31 +0100

    [FIX] Fixed StyleInd.ist
    - Force StyleInd.ist file to be commited
    - Edited building script

--- report/build.sh
+++ report/build.sh
@@ -4,7 +4,7 @@ OUTPUT_DIRECTORY="build"
 echo "Building ..."
 mkdir "./$OUTPUT_DIRECTORY"
 pdflatex -output-directory=$OUTPUT_DIRECTORY --shell-escape $FILE_NAME_INPUT
-makeindex "./$OUTPUT_DIRECTORY/$FILE_NAME_INPUT.idx" -s "./$OUTPUT_DIRECTORY/StyleInd.ist"
+makeindex "./$OUTPUT_DIRECTORY/$FILE_NAME_INPUT.idx" -s "./StyleInd.ist"
 biber -aux-directory=$OUTPUT_DIRECTORY main
 pdflatex -output-directory=$OUTPUT_DIRECTORY --shell-escape $FILE_NAME_INPUT
 
