8771336e13301c16d87bae87bf78e52829feffdf
BOT_Axel <axellebot@gmail.com>
2018-01-21 00:31:09 +0100

    [TASK] Edited cpp/tp7+tp8 sources
    -  Edited comments

--- sources/cpp/TP7-8/clientUDP.c
+++ sources/cpp/TP7-8/clientUDP.c
@@ -17,9 +17,10 @@
 #define MESSAGE_A_ENVOYER ".eruelp tnafne nu ,siofrap te ,erbmos etef enu'd engapmahc ed sellub sel emmoc tnellitnics seliote sel uo egenam nu tse ednom el"
 #define TAILLE_MESSAGE 128 // important d'etre exact, sinon probleme a l'inversion du buffer
 
-int main(int argc, const char *argv[]){
+int main(int argc, const char *argv[]) {
     /* le nombre d'arguments doit etre de 2 exactement */
-    if(argc != 3){
+    if (argc != 3) {
+        perror("erreur argument");
         printf("usage : %s ip_server port\n", argv[0]);
         printf("l'ip du serveur UDP sous forme xxx.xxx.xxx.xxx\n");
         printf("le numéro du port à écouter, entre 1 et 65535\n");
@@ -28,7 +29,7 @@ int main(int argc, const char *argv[]){
 
     // Etape 0 : definition des variables
     int sock;   // socket en mode non connecte UDP
-    struct sockaddr_in AdresseServeur, rcptAdrr; // carte de visite contenant l'identite du serveur
+    struct sockaddr_in serverAddr, rcptAdrr; // carte de visite contenant l'identite du serveur
     socklen_t longueurStructAdresse; // contient la taille d'une carte de visite
     ssize_t nbSend; // stockage du nombre d'octets recus
     ssize_t recus;
@@ -40,32 +41,29 @@ int main(int argc, const char *argv[]){
 
     // Etape 1 : creation de la socket de communication
     sock = socket(PF_INET, SOCK_DGRAM, 0);
-    if(sock == -1){
-        perror("Erreur création socket\n");
+    if (sock == -1) {
+        perror("erreur socket()\n");
     }
 
     // Etape 2 : creation d'une structure type sockaddr_in contenant l'identite du serveur
-    // identite composee d'une association IP-port
-    AdresseServeur.sin_family = AF_INET; // famille d’adresse IPv4
-    inet_aton(argv[1], &AdresseServeur.sin_addr); // IP du serveur
-    // Port du serveur relie à l'application :
-    AdresseServeur.sin_port = htons(atoi(argv[2]));
+    serverAddr.sin_family = AF_INET; // famille d’adresse IPv4
+    inet_aton(argv[1], &serverAddr.sin_addr); // IP du serveur
+    serverAddr.sin_port = htons(atoi(argv[2]));// Port du serveur relie à l'application
 
-    // Etape 3 : envoi du message
-    // envoi vers serveur (identite contenue dans AdresseServeur
-    nbSend = sendto(sock, buffer, TAILLE_MESSAGE, 0, (struct sockaddr *) &AdresseServeur, longueurStructAdresse);
-    if(nbSend == -1){
+    // Etape 3 : envoi du message au serveur
+    nbSend = sendto(sock, buffer, TAILLE_MESSAGE, 0, (struct sockaddr *) &serverAddr, longueurStructAdresse);
+    if (nbSend == -1) {
         perror("erreur sendto()");
         exit(1);
     }
-    printf("message envoye\n");
+    printf("message sent\n");
 
     //Etape 4 : attente du retour du serveur
     recus = recvfrom(sock, buffer2, 1024, 0, (struct sockaddr *) &rcptAdrr, &longueurStructAdresse);
-    if(recus == -1){
+    if (recus == -1) {
         perror("erreur recvfrom()");
         exit(1);
     }
-    printf("message reçu : %s \n", buffer2);
+    printf("message received : %s \n", buffer2);
     close(sock);
 }
--- sources/cpp/TP7-8/serveurTCP.c
+++ sources/cpp/TP7-8/serveurTCP.c
@@ -3,8 +3,8 @@
 //
 
 /*
- nom : ServeurUDPEcoute
- description : écoute en continu les messages UDP sur un port donné (ex 5000) en IPV4
+ nom : ServeurTCP
+ description : écoute en continu les messages TCP sur un port donné (ex 5000) en IPV4
  affiche le message recu ainsi que l'identité de l'émetteur
  auteur : Axel LE BOT
  */
@@ -14,9 +14,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <errno.h>
 #include <string.h>
 
 #define SERVER_PORT 4444
@@ -28,83 +25,74 @@ void inverser_buffer(char *bufferainverser, int taille) {
     // precision : utiliser la taille du message, pas celle du buffer
     int i;
     char temp;
-    for (i=0; i < taille/2 ; i++) {
+    for (i = 0; i < taille / 2; i++) {
         temp = bufferainverser[i];
-        bufferainverser[i]= bufferainverser[taille-1-i];
-        bufferainverser[taille-i-1] = temp;
+        bufferainverser[i] = bufferainverser[taille - 1 - i];
+        bufferainverser[taille - i - 1] = temp;
     }
 }
 
-int main (int argc, char **argv) {
+int main(int argc, char **argv) {
 
-   int sp;  /* descripteur de la socket principale */
-   int st;  /* descripteur de la socket de travail */
-   int nbRecus; /* entier utilisé lors de la lecture d'un message */
+    int sp;  // descripteur de la socket principale
+    int st;  // descripteur de la socket de travail
+    struct sockaddr_in serverAddr; // pour le stockage de sa propre identite (serveur)
+    struct sockaddr_in cliAddr; // pour le stockage de l'identité du client
+    socklen_t cliSize; // pour le stockage de l'adresse du client
 
-   struct sockaddr_in servAddr; /* structure pour renseigner l'adresse du serveur et l'associer à la socket */
-   struct sockaddr_in cliAddr; /* structure pour stocker l'identité du client qui a envoyé un message recu*/   
-   socklen_t cliSize; /* type utilisé pour récupérer la taille de l'adresse du client qui a envoyé un message recu */
-   char buffer[MAXBUFFERSIZE]; /* buffer pour lire puis afficher les messages recus*/   
+    ssize_t nbRecus; // stockage du nombre d’octets recus
+    char buffer[MAXBUFFERSIZE]; // buffer pour lecture du message
 
-   /**********************************************/
-   /* Création du support de communication       */
-   /**********************************************/
-
-   /* Création d'une socket. Reseau internet (PF_INET) , Transport : TCP (SOCK_STREAM) */
-   if ((sp = socket(PF_INET, SOCK_STREAM, 0 )) == -1) {
-      perror("création de la socket");
-      exit(1);
-   }
-
-   /* Nous renseignons la structure servAddr avec les éléments du serveur : type de socket, adresse ip, port */
-   servAddr.sin_family = AF_INET;   /* Adresse internet de type IPv4 */   
-   servAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Permet d'associer toutes les ips de la machine serveur */
-   servAddr.sin_port = htons(SERVER_PORT); /* sur quel port écouter */
-    
-
-   /* Nous associons la socket locale à l'identité du serveur (contenue dans la structure servAddr) */
-   if (bind(sp, (struct sockaddr *) &servAddr, sizeof(servAddr)) == -1) {
-      perror("association socket / adresse");
-      fprintf(stderr, "Ceci arrive généralement si :\n");
-      fprintf(stderr, "- le port est déjà utilisé par une autre application\n");
-      fprintf(stderr, "- vous n'avez pas l'autorisation d'utiliser ce port (réservé systeme)\n");
-      fprintf(stderr, "- la derniere instance du programme a mal fermé la socket\n");
-      exit(1);
-   } 
-  
-
-   /********************************************************/
-   /* etablisement et gestion du dialogue client / serveur */
-   /********************************************************/
-
-/* on met la socket principale  en position d'écoute (necessaire uniquement pour TCP)*/
-listen(sp, 1);	
-/*boucle infinie : nous écoutons les connexions entrantes sur la socket principale */
-while (1) {
-    /* mode écoute : on attend l'arrivee d'une connexion entrante */
-    if ((st = accept(sp, (struct sockaddr *) &cliAddr, &cliSize))==-1) {
-        perror("problem accept");
-        exit(1); 
-    }       
-    /* une connexion entrante s'est produite.
-    /* cliAddr a été rempli avec les informations d'identification du client*/
-    /* on lit alors sur cette socket un message  */
-    if ((nbRecus = recv(st, buffer, MAXBUFFERSIZE, 0)) == -1) {
-        perror("problem recv");
+    // Etape 1 : creation de la socket de communication
+    sp = socket(PF_INET, SOCK_STREAM, 0);
+    if (sp == -1) {
+        perror("Erreur création socket\n");
         exit(1);
     }
-    /* nbRecus contient le nombre d'octets qui ont été lus  (au max : MAXBUFFERSIZE)*/ 
-    /* buffer contient le message recu */
-    // on inverse le buffer :
-    inverser_buffer(buffer, strlen(buffer));
-    // ajout du caractère de fin de buffer :
-    buffer[nbRecus]='\0';
-    //On renvoie le buffer inversé au client :
-    if (sendto(st, buffer, strlen(buffer), 0, (struct sockaddr*) &cliAddr, cliSize) ==-1) {
-        perror("erreur sendto()");
+
+    // Etape 2 : creation d'une structure type sockaddr_in contenant sa propre identite
+    serverAddr.sin_family = AF_INET;   // famille d’adresse IPv4
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); // n'importe quelle IP
+    serverAddr.sin_port = htons(SERVER_PORT); // Port a associer
+
+    // Etape 3 : association de l'adresse avec la socket
+    if (bind(sp, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) == -1) {
+        perror("erreur 1 bind()");
         exit(1);
     }
-    /* on ferme enfin la socket de travail */
-    close(st);
-}
+
+    // Etape 4 : mise en route d'une boucle continue.
+    // Ecoute sur la socket.
+    listen(sp, 1);
+    //boucle infinie : nous écoutons les connexions entrantes sur la socket principale
+    while (1) {
+        // mode écoute : on attend l'arrivee d'une connexion entrante
+        if ((st = accept(sp, (struct sockaddr *) &cliAddr, &cliSize)) == -1) {
+            perror("erreur 2 accept()");
+            exit(1);
+        }
+
+        // réception d'un message
+        // message stocké dans buffer
+        // on lit alors sur cette socket un message
+        nbRecus = recv(st, buffer, MAXBUFFERSIZE, 0);
+        if (nbRecus == -1) {
+            perror("erreur recv()");
+            exit(1);
+        }
+
+        // on inverse le buffer :
+        inverser_buffer(buffer, strlen(buffer));
+        // ajout du caractère de fin de buffer :
+        buffer[nbRecus] = '\0';
+
+        //On renvoie le buffer inversé au client :
+        if (sendto(st, buffer, strlen(buffer), 0, (struct sockaddr *) &cliAddr, cliSize) == -1) {
+            perror("erreur sendto()");
+            exit(1);
+        }
+
+        // on ferme enfin la socket de travail
+        close(st);
+    }
 }
